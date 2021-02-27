6957f6640d78148a297f217ca831ba54a56b2fee
Marko Marković <you@example.com>
2021-02-27 15:09:20 +0100

    tic tac toe

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+//import Game from "./components/Game";
+import App from "./App";
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDom.render(<App />, document.getElementById("root"));
