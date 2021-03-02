5fe1fe961fff147e801f8ce6e21119cd8bdfa416
Marko Marković <you@example.com>
2021-03-02 10:55:58 +0100

    added external json server

--- my-app/src/service.js
+++ my-app/src/service.js
@@ -1,6 +1,8 @@
 //service.js
+// host through github 'https://my-json-server.typicode.com/Marko2225/users-api/users
+
 import axios from 'axios'
 
-export const getAllUsers = () => axios.get('http://localhost:3005/users')
+export const getAllUsers = () => axios.get('https://my-json-server.typicode.com/Marko2225/users-api/users')
 
 export const registerUser = (username,email,password) => axios.post('http://localhost:3005/users',{username,email,password})
